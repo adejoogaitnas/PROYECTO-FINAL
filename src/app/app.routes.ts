fe1540f6677826f4d6ca66f629ec57c2af12fee8
adejoogaitnas <adejoogaitnas@gmail.com>
2024-03-06 20:11:15 +0100

    ADIVINANZA ACTIVO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { ClimaComponent } from './pages/clima/clima.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GithubComponent } from './pages/github/github.component';
 import { PeliculasComponent } from './pages/peliculas/peliculas.component';
+import { AdivinanzaComponent } from './pages/adivinanza/adivinanza.component';
 
 export const routes: Routes = [
 
     {path: '', component:HomeComponent},
     { path: 'clima', component: ClimaComponent },
     { path: 'github', component: GithubComponent },
-    { path: 'peliculas', component: PeliculasComponent},
+    { path: 'peliculas', component: PeliculasComponent },
+    { path: 'adivinanza', component: AdivinanzaComponent },
 
     { path: '**', redirectTo: '', pathMatch: 'full' },
     
